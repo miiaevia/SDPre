8e1133ca91ffaeaed29df41c336a3f90a59e1c1d
miiaevia <miiaevia@gmail.com>
2018-02-07 09:44:49 -0700

    add exercise 6

---
+++ JavaTheHardWay/MathOperations.java
@@ -0,0 +1,39 @@
+public class MathOperations {
+	public static void main ( String[] args ) {
+		int a, b, c, d, e, f, g, h; 
+		double w, x, y, z; 
+		String one, two, both; 
+
+		a = 10; 
+		b = 27; 
+		System.out.println( "a is " + a + ", b is " + b ); 
+
+		c = a + b; 
+		System.out.println( "a+b is " + c ); 
+		d = a - b; 
+		System.out.println( "a-b is " + d ); 
+		e = a + b * 3; 
+		System.out.println( "a+b*3 is " + e ); 
+		f = b / 2; 
+		System.out.println( "b/2 is " + f ); 
+		g = b % 10; 
+		System.out.println( "b%10 is " + g ); 
+		h = g + a - b * c / d % e; 
+		System.out.println( "g + a - b * c / d % e = " + h);
+
+		w = 3.14 * 3.14;
+		System.out.println( "The area of a circle is the radius * " + w);
+		x = 1.1; 
+		System.out.println( "\nx is " + x ); 
+		y = x * x; 
+		System.out.println( "x*x is " + y ); 
+		z = b / 2; 
+		System.out.println( "b/2 is " + z ); 
+		System.out.println(); 
+
+		one = "dog"; 
+		two = "house"; 
+		both = one + two; 
+		System.out.println( both ); 
+	}
+}
