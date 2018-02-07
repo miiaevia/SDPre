7b82bebec79c9cbaef8af12f88a90e734d691e68
miiaevia <miiaevia@gmail.com>
2018-02-07 09:29:53 -0700

    add exercise 1

---
+++ JavaTheHardWay/FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+	public static void main( String[] args ) {
+		System.out.println( "I, Megan Arnold, am determined to learn how to code." );
+		System.out.println( "Today's date is November 9, 2017." );
+	}
+}
