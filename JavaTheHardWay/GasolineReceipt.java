9717ae39670e6a5a5bf55e3890e65497a0704a4f
miiaevia <miiaevia@gmail.com>
2018-02-07 09:34:11 -0700

    add exercise 2

---
+++ JavaTheHardWay/GasolineReceipt.java
@@ -0,0 +1,16 @@
+public class GasolineReceipt {
+	public static void main( String[] args) {
+		System.out.println("+------------------------+");
+		System.out.println("|                        |");
+		System.out.println("|      CORNER STORE      |"); 
+		System.out.println("|                        |"); 
+		System.out.println("|  2017-11-09  06:00PM   |"); 
+		System.out.println("|                        |"); 
+		System.out.println("|  Gallons:       14.870 |"); 
+		System.out.println("|  Price/gallon: $ 2.805 |"); 
+		System.out.println("|                        |"); 
+		System.out.println("|  Fuel total:  $ 41.71  |");
+		System.out.println("|                        |"); 
+		System.out.println("+------------------------+"); 
+	}
+}
