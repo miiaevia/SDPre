d75a74ebe4eb7db787604a5b1ead97d3c4acb8d2
miiaevia <miiaevia@gmail.com>
2018-02-09 18:56:02 -0700

    add O6Lab4

---
+++ Labs/Objective6Lab4.java
@@ -0,0 +1,11 @@
+public class Objective6Lab4 {
+  public static void main( String[] args ) {
+    int sum = 0 , count = 0;
+
+    while (count <= 19) {
+      count++;
+      sum += count;
+    }
+    System.out.print(sum);
+  }
+}
