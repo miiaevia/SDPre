617d6086dcbc6ebdefb4f2b22b6233d419611308
miiaevia <miiaevia@gmail.com>
2018-02-10 08:36:29 -0700

    add O6Lab2

---
+++ Labs/Objective6Lab2.java
@@ -0,0 +1,10 @@
+public class Objective6Lab2 {
+  public static void main(String[] args) {
+  int count = 0;
+
+    while(count <= 19) {
+      count++;
+      System.out.println(count);
+    }
+  }
+}
