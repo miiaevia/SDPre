eb3e515c40347b4dbde0740b9ea1516c7c7e4f88
miiaevia <miiaevia@gmail.com>
2018-02-11 09:23:19 -0700

    add O3Lab3

---
+++ Labs/Objective3Lab3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Objective3Lab3 {
+  public static void main(String[] args) {
+    int birthYear, age, currentYear;
+
+    birthYear = 0;
+    currentYear = 2018;
+
+    Scanner input = new Scanner(System.in);
+    System.out.println("How old will you turn this year?");
+    age = input.nextInt();
+
+    birthYear = currentYear - age;
+    System.out.println("You were born in " + birthYear);
+
+    input.close();
+  }
+}
