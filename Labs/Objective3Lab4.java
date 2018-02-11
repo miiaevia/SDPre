9a2c627510540921c46a4f1322bf6079289760c9
miiaevia <miiaevia@gmail.com>
2018-02-11 09:14:07 -0700

    add O3Lab4

---
+++ Labs/Objective3Lab4.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Objective3Lab4 {
+  public static void main(String[] args) {
+    String firstName;
+    String lastName;
+    int age;
+
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("What is your first name?");
+    firstName = input.next();
+
+    System.out.println("What is your last name?");
+    lastName = input.next();
+
+    System.out.println("What is your age?");
+    age = input.nextInt();
+
+    System.out.print("First Name: " + firstName + ", Last Name: " + lastName);
+    System.out.println(", Age: " + age);
+
+    input.close();
+  }
+}
