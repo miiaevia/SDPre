55f17adba2556e63e785c91fb1f54d906b3d1324
miiaevia <miiaevia@gmail.com>
2018-02-11 09:53:07 -0700

    add O1Lab2

---
+++ Labs/Objective1Lab2.java
@@ -0,0 +1,9 @@
+public class Objective1Lab2{
+  public static void main( String[] args) {
+    int age = 29;
+    String name = "Megan Arnold";
+
+    System.out.println(name);
+    System.out.println(age);
+  }
+}
