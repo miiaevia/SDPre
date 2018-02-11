e73e6cb597a5c47e39bbd0e314eba83d1a5ffac3
miiaevia <miiaevia@gmail.com>
2018-02-11 09:50:51 -0700

    add O1Lab3

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,10 @@
+public class Objective1Lab3 {
+  public static void main( String[] args) {
+    int age = 29;
+    String name = "Megan", state = "Texas";
+
+    System.out.println("Name: " + name);
+    System.out.println("Age: " + age);
+    System.out.println("State: " + state);
+  }
+}
