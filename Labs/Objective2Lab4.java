6d8a796ecd51da1afd0d7011ef081143f5e18753
miiaevia <miiaevia@gmail.com>
2018-02-11 09:40:34 -0700

    add O2Lab4

---
+++ Labs/Objective2Lab4.java
@@ -0,0 +1,13 @@
+public class Objective2Lab4 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+
+    num1 = 15;
+    num2 = 8;
+    num3 = 21;
+
+    average = (num1 + num2 + num3)/3;
+    System.out.print("The average of " + num1 + ", " + num2 + ", and " + num3);
+    System.out.println(" is: " + average);
+  }
+}
